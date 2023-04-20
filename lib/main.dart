604fb101ebbcdb8a062d90a08ce9a10b01af170a
volkan <volkanilkyar@gmail.com>
2023-04-21 00:40:07 +0300

    first day codes

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:calculate_dynamic_average/constants/constants.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Ortalama Hesapla',
+      theme: ThemeData(primaryColor: Constants.mainColor),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Material App Bar'),
+        ),
+        body: const Center(
+          child: Text('Hello World'),
+        ),
+      ),
+    );
+  }
+}
